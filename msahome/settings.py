779ec851c5015366c279214d71c98032a0af6fff
ychong4 <65525074+ychong4@users.noreply.github.com>
2020-12-17 13:31:48 -0600

    Update settings.py

--- msahome/settings.py
+++ msahome/settings.py
@@ -79,10 +79,8 @@ WSGI_APPLICATION = 'msahome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'msasoccer',
-        'USER': 'postgres',
-        'PASSWORD': 'lahenkenius'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
